56b564f25ca7e21fdff25fb36f21fe386298356c
Z0R7N <46117076+Z0R7N@users.noreply.github.com>
2021-06-23 13:10:56 +0300

    update store_page.py add debug messages

--- pages/store_page.py
+++ pages/store_page.py
@@ -1,3 +1,5 @@
+import time
+
 from pages.base_page import BasePage
 from pages.locators import StorePageLocators
 
@@ -5,12 +7,17 @@ from pages.locators import StorePageLocators
 class StorePage(BasePage):
     def should_be_guest_can_add_item_to_cart(self):
         self.should_be_guest_can_accept_region_location()
-        self.should_be_guest_can_accept_cookie()
+        print("region accepted")
+        time.sleep(3)
+        # self.should_be_guest_can_accept_cookie()
+        print("cookie accepted")
+        time.sleep(30)
         self.should_be_guest_can_choose_group_store()
         self.should_be_guest_can_click_item_link()
 
     def should_be_guest_can_accept_region_location(self):
-        self.browser.find_element(*StorePageLocators.ACCEPT_LOCATION).click()
+        if self.is_element_present(StorePageLocators.ACCEPT_LOCATION):
+            self.browser.find_element(*StorePageLocators.ACCEPT_LOCATION).click()
 
     def should_be_guest_can_accept_cookie(self):
         self.browser.find_element(*StorePageLocators.ACCEPT_COOKIE).click()
