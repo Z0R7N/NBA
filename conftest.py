50ddcb5bb28bb4bef22b2e428dc9cd25760ac915
Dmitriy Andreev <dmtrii.a@gmail.com>
2021-06-28 12:46:06 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -5,10 +5,12 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 
+# function for choosing browser google chrome or firefox
 def pytest_addoption(parser):
     parser.addoption('--browser_name', action='store', default='chrome', help='Choose browser: chrome or firefox')
 
 
+# function to launch selected browser with url and close browser after done work
 @pytest.fixture(scope="function")
 def browser(request):
     browser_name = request.config.getoption("browser_name")
